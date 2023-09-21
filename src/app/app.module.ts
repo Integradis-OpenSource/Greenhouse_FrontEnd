43a6dc9b503d6d74ab965d32be9d3e63c934e365
CarloLSG <u20211a475@upc.edu.pe>
2023-09-20 23:41:21 -0500

    chore: import components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {InformationComponent} from "./profiles/components/information/information.component";
+import {ProfileInformationComponent} from "./profiles/components/profile-information/profile-information.component";
+import { MainContentComponent } from './profiles/components/main-content/main-content.component';
+import {AvatarComponent} from "./profiles/components/avatar/avatar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InformationComponent,
+    ProfileInformationComponent,
+    MainContentComponent,
+    AvatarComponent
   ],
   imports: [
     BrowserModule,
