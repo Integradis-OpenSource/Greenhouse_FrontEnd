2fc004d3b464aedd35b9b2b5864c5f5812270d3f
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-09-22 18:36:54 -0500

    feat(navbar): Added routing between navbar and organization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {DashboardContentComponent} from "./dashboard/components/dashboard-content/dashboard-content.component";
 import {StepperContentComponent} from "./harvestings/components/stepper-content/stepper-content.component";
 import {ProfileContentComponent} from "./profiles/users/components/profile-content/profile-content.component";
+import { OrganizationContentComponent } from "./profiles/organizations/components/organization-content/organization-content.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardContentComponent },
   { path: 'process-log', component: StepperContentComponent },
   { path: 'profile', component: ProfileContentComponent },
+  { path: 'organization', component: OrganizationContentComponent },
 ];
 
 @NgModule({
