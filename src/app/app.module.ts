af7f5d8268d0cb2ef43c9cfd1b802931e70a8367
CarloLSG <u20211a475@upc.edu.pe>
2023-09-21 22:49:29 -0500

    chore: update declarations and imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {InformationComponent} from "./profiles/components/information/information.component";
-import {ProfileInformationComponent} from "./profiles/components/profile-information/profile-information.component";
-import { ProfileContentComponent } from './profiles/components/profile-content/profile-content.component';
-import {AvatarComponent} from "./profiles/components/avatar/avatar.component";
+import {MatCardModule} from "@angular/material/card";
+import {NgOptimizedImage} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
+import {InformationComponent} from "./profiles/shared/components/information/information.component";
+import {ProfileInformationComponent} from "./profiles/users/components/profile-information/profile-information.component";
+import {ProfileContentComponent} from "./profiles/users/components/profile-content/profile-content.component";
+import {AvatarComponent} from "./profiles/shared/components/avatar/avatar.component";
+import {OrganizationContentComponent} from "./profiles/organizations/components/organization-content/organization-content.component";
+import {OrganizationInformationComponent} from "./profiles/organizations/components/organization-information/organization-information.component";
+import {EmployeesContentComponent} from "./profiles/employees/components/employees-content/employees-content.component";
+
 
 @NgModule({
   declarations: [
@@ -15,12 +22,18 @@ import {AvatarComponent} from "./profiles/components/avatar/avatar.component";
     InformationComponent,
     ProfileInformationComponent,
     ProfileContentComponent,
-    AvatarComponent
+    AvatarComponent,
+    OrganizationContentComponent,
+    OrganizationInformationComponent,
+    EmployeesContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    NgOptimizedImage,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
