7c3a4c3bab71d2be010faef0a778aa3f4278475d
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-09-22 01:53:29 -0500

    feat(navbar): Added routing between navbar and profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardContentComponent} from "./dashboard/components/dashboard-content/dashboard-content.component";
 import {StepperContentComponent} from "./harvestings/components/stepper-content/stepper-content.component";
+import {ProfileContentComponent} from "./profiles/users/components/profile-content/profile-content.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardContentComponent },
   { path: 'process-log', component: StepperContentComponent },
+  { path: 'profile', component: ProfileContentComponent },
 ];
 
 @NgModule({
