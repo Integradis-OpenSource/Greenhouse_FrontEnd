16d40e86e7e45c0ca0f5c5a3cb580f7cfc419a59
Jess_2610 <105735491+Mei0202@users.noreply.github.com>
2023-09-23 03:51:30 -0500

    feat<event-service>:Add EventService for object activation

--- src/app/harvestings/services/event.service.ts
+++ src/app/harvestings/services/event.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { Subject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class EventService {
+export class EventServiceService {
+  private addObjectSubject = new Subject<void>();
 
-  constructor() { }
+  activateObject() {
+    this.addObjectSubject.next();
+  }
 }
+
