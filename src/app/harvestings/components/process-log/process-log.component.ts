767d626abdbb25191e4226d11736d3ccd4a36f58
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-09-22 23:52:00 -0500

    feat(process-log): Added process log component

---
+++ src/app/harvestings/components/process-log/process-log.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process-log',
+  templateUrl: './process-log.component.html',
+  styleUrls: ['./process-log.component.css']
+})
+export class ProcessLogComponent {
+
+}
