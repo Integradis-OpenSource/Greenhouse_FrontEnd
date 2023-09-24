52df7a6d0c4adeef6d415d374c3663dde9608976
Jess_2610 <105735491+Mei0202@users.noreply.github.com>
2023-09-23 20:00:53 -0500

    Css added,path in app router /popup-crop-finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { OrganizationContentComponent } from "./profiles/organizations/component
 import {ProcessLogComponent} from "./harvestings/components/process-log/process-log.component";
 import {PopupWarningComponent} from "./harvestings/components/popup-warning/popup-warning.component";
 import {PopupNewCropComponent} from "./harvestings/components/popup-new-crop/popup-new-crop.component";
+import {PopupCropFinishedComponent} from "./harvestings/components/popup-crop-finished/popup-crop-finished.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -16,7 +17,8 @@ const routes: Routes = [
   { path: 'profile', component: ProfileContentComponent },
   { path: 'organization', component: OrganizationContentComponent },
   {path:'popup',component:PopupWarningComponent},
-  {path:'popup-new-crop', component:PopupNewCropComponent}
+  {path:'popup-new-crop', component:PopupNewCropComponent},
+  {path:'popup-crop-finished',component:PopupCropFinishedComponent}
 ];
 
 @NgModule({
