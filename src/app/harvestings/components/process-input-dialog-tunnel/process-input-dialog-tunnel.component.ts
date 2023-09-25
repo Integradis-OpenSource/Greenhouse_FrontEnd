481712b7d722affa093fd8b5757499df0c82a55d
CarloLSG <u20211a475@upc.edu.pe>
2023-09-24 21:24:41 -0500

    chore(bunker): Post data form the tunnel form into the tunnel table.

--- src/app/harvestings/components/process-input-dialog-tunnel/process-input-dialog-tunnel.component.ts
+++ src/app/harvestings/components/process-input-dialog-tunnel/process-input-dialog-tunnel.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
+import {ProcessEntriesService} from "../../services/process-entries.service";
 
 @Component({
   selector: 'app-process-input-dialog-tunnel',
@@ -17,9 +18,36 @@ export class ProcessInputDialogTunnelComponent {
   tp: number = 0;
   growRoom: string = '';
   comment: string = '';
-  constructor(public dialogRef: MatDialogRef<ProcessInputDialogTunnelComponent>) {
+  constructor(
+    private tunnelService: ProcessEntriesService,
+    public dialogRef: MatDialogRef<ProcessInputDialogTunnelComponent>
+  ) {
+    this.tunnelService.setResourceEndpoint('bunker');
   }
   onCancelClick(): void {
+    this.dialogRef.close();
+  }
+  onSaveClick() {
+    const formData = {
+      author: this.author,
+      day: this.day,
+      date: this.date,
+      time: this.time,
+      t1: this.t1,
+      t2: this.t2,
+      t3: this.t3,
+      tp: this.tp,
+      growRoom: this.growRoom,
+      comment: this.comment,
+    }
+    this.tunnelService.create(formData).subscribe(
+      (response) => {
+        console.log('Data saved successfully:', response);
+      },
+      (error) => {
+        console.error('Error saving data:', error);
+      }
+    );
     this.dialogRef.close();
   }
 }
