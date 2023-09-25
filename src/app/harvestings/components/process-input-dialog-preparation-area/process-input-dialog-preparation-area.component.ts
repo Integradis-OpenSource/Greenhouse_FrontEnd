32900bede33ee890258da45e2a57c905b9752195
CarloLSG <u20211a475@upc.edu.pe>
2023-09-24 20:55:46 -0500

    chore(preparation-area): Post data form the preparation-area form into the preparation-area table.

--- src/app/harvestings/components/process-input-dialog-preparation-area/process-input-dialog-preparation-area.component.ts
+++ src/app/harvestings/components/process-input-dialog-preparation-area/process-input-dialog-preparation-area.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
+import {ProcessEntriesService} from "../../services/process-entries.service";
 
 @Component({
   selector: 'app-process-input-dialog-preparation-area',
@@ -14,9 +15,35 @@ export class ProcessInputDialogPreparationAreaComponent {
   activities: string = '';
   temperature: number = 0;
   comment: string = '';
-  constructor(public dialogRef: MatDialogRef<ProcessInputDialogPreparationAreaComponent>) {
+  constructor(
+    private stockService: ProcessEntriesService,
+    public dialogRef: MatDialogRef<ProcessInputDialogPreparationAreaComponent>
+  ) {
+    this.stockService.setResourceEndpoint('preparation_area');
   }
   onCancelClick(): void {
+    this.dialogRef.close();
+  }
+  onSaveClick() {
+    const formData = {
+      author: this.author,
+      day: this.day,
+      date: this.date,
+      time: this.time,
+      activities: this.activities,
+      temperature: this.temperature,
+      comment: this.comment,
+    };
+    this.stockService.create(formData).subscribe(
+      (response) => {
+        console.log('Data saved successfully:', response);
+      },
+      (error) => {
+        console.error('Error saving data:', error);
+      }
+    );
+
+    // Close the dialog
     this.dialogRef.close();
   }
 }
