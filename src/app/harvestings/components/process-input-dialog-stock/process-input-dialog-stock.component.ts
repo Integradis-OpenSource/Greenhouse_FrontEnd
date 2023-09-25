3c6cfd2f7ff0bd661745d7a094448f87a18947ef
CarloLSG <u20211a475@upc.edu.pe>
2023-09-24 20:55:20 -0500

    chore(stock): Post data form the stock form into the stock table.

--- src/app/harvestings/components/process-input-dialog-stock/process-input-dialog-stock.component.ts
+++ src/app/harvestings/components/process-input-dialog-stock/process-input-dialog-stock.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import {ProcessEntriesService} from "../../services/process-entries.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-process-input-dialog-stock',
@@ -19,9 +21,44 @@ export class ProcessInputDialogStockComponent {
   gypsum: number = 0;
   urea: number = 0;
   ammoniumSulphate: number = 0;
-  constructor(public dialogRef: MatDialogRef<ProcessInputDialogStockComponent>) {
+
+  constructor(
+    private stockService: ProcessEntriesService,
+    public dialogRef: MatDialogRef<ProcessInputDialogStockComponent>
+  ) {
+    this.stockService.setResourceEndpoint('stock');
   }
+
   onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSaveClick() {
+    const formData = {
+      author: this.author,
+      day: this.day,
+      date: this.date,
+      time: this.time,
+      hay: this.hay,
+      corn: this.corn,
+      guano: this.guano,
+      cottonSeedCake: this.cottonSeedCake,
+      soybeanMeal: this.soybeanMeal,
+      gypsum: this.gypsum,
+      urea: this.urea,
+      ammoniumSulphate: this.ammoniumSulphate,
+    };
+
+    this.stockService.create(formData).subscribe(
+      (response) => {
+        console.log('Data saved successfully:', response);
+      },
+      (error) => {
+        console.error('Error saving data:', error);
+      }
+    );
+
+    // Close the dialog
     this.dialogRef.close();
   }
 }
