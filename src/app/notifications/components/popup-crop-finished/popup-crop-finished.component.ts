8db7c753ccb59caee728171be92adb3ba8ffd1ef
Nicolas <nespinoza.naer@gmail.com>
2023-09-25 10:50:58 -0500

    chore: Connected popup components to main components

--- src/app/notifications/components/popup-crop-finished/popup-crop-finished.component.ts
+++ src/app/notifications/components/popup-crop-finished/popup-crop-finished.component.ts
@@ -1,7 +1,7 @@
 import { Component,Input } from '@angular/core';
-import {EventServiceService} from "../../services/event.service";
+import {EventServiceService} from "../../../harvestings/services/event.service";
 import { Router } from '@angular/router';
-import {CommunicationService} from "../../services/communication.service";
+import {CommunicationService} from "../../../harvestings/services/communication.service";
 @Component({
   selector: 'app-popup-crop-finished',
   templateUrl: './popup-crop-finished.component.html',
@@ -9,10 +9,12 @@ import {CommunicationService} from "../../services/communication.service";
 })
 export class PopupCropFinishedComponent {
   @Input() popupText = 'The cultivation started on '
-  @Input() date_start= "18/09/23 "
+  date_start= Date.now()
   @Input() popupText2= `has successfully completed all stages, the records were filed in the section `;
   @Input() popupText3= '"Control Panel", Crop History"'
   popupVisible = false;
+  notificationVisible = false;
+
 
   constructor(private interactionService: EventServiceService, private router: Router,private communicationService: CommunicationService) {
 
@@ -21,6 +23,12 @@ export class PopupCropFinishedComponent {
   activeObjectPopUpCropFinished() {
     this.interactionService.activeObjectPopUpCropFinished();
     this.popupVisible = true;
+  }
+
+  activeObjectNotificationFinished() {
+    this.interactionService.activeObjectPopUpCropFinished();
+    this.popupVisible = false;
+    this.notificationVisible = true;
   }
   closePopUpCropFinished() {
     this.popupVisible = false;
