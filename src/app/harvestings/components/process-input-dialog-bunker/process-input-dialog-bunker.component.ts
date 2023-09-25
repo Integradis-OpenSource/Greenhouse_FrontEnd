857227b734f6c9366ae51170ecc027badcbca671
CarloLSG <u20211a475@upc.edu.pe>
2023-09-24 21:24:26 -0500

    chore(bunker): Post data form the bunker form into the bunker table.

--- src/app/harvestings/components/process-input-dialog-bunker/process-input-dialog-bunker.component.ts
+++ src/app/harvestings/components/process-input-dialog-bunker/process-input-dialog-bunker.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatDialogRef} from "@angular/material/dialog";
+import {ProcessEntriesService} from "../../services/process-entries.service";
 
 @Component({
   selector: 'app-process-input-dialog-bunker',
@@ -17,9 +18,39 @@ export class ProcessInputDialogBunkerComponent {
   tp: number = 0;
   freq: number = 0;
   comment: string = '';
-  constructor(public dialogRef: MatDialogRef<ProcessInputDialogBunkerComponent>) {
+  constructor(
+    private bunkerService: ProcessEntriesService,
+    public dialogRef: MatDialogRef<ProcessInputDialogBunkerComponent>
+  ) {
+    this.bunkerService.setResourceEndpoint('bunker');
   }
   onCancelClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSaveClick() {
+    const formData = {
+      author: this.author,
+      day: this.day,
+      date: this.date,
+      time: this.time,
+      t1: this.t1,
+      t2: this.t2,
+      t3: this.t3,
+      tp: this.tp,
+      freq: this.freq,
+      comment: this.comment,
+    }
+    this.bunkerService.create(formData).subscribe(
+      (response) => {
+        console.log('Data saved successfully:', response);
+      },
+      (error) => {
+        console.error('Error saving data:', error);
+      }
+    );
+
+    // Close the dialog
     this.dialogRef.close();
   }
 }
