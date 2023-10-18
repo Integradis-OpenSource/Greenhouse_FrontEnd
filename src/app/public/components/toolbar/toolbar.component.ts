90f076590a71b89e871960aa47352557ec2db772
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-10-17 22:27:42 -0500

    chore(locales): Added styles to dropdown language selector

--- src/app/public/components/toolbar/toolbar.component.ts
+++ src/app/public/components/toolbar/toolbar.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
+  organization_name = 'placeholder_organization_name';
+  user_name = 'placeholder_user_name';
   showMenu = false;
   toggleNavbar(){
     this.showMenu = !this.showMenu;
