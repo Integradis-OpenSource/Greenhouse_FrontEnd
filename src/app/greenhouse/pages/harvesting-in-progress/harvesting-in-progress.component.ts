29c98f723d58d8b04f989460b96c55d964d9c083
Nicolas <nespinoza.naer@gmail.com>
2023-10-23 12:17:59 -0500

    feat(harvesting-in-progress): added crops in progress table

--- src/app/greenhouse/pages/harvesting-in-progress/harvesting-in-progress.component.ts
+++ src/app/greenhouse/pages/harvesting-in-progress/harvesting-in-progress.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Crop} from "../../model/crop";
+import {CropsService} from "../../services/crops.service";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-harvesting-in-progress',
   templateUrl: './harvesting-in-progress.component.html',
   styleUrls: ['./harvesting-in-progress.component.css']
 })
-export class HarvestingInProgressComponent {
+export class HarvestingInProgressComponent implements OnInit {
+  dataSource: MatTableDataSource<Crop>;
+  displayedColumns: string[] = ["id","start_date","phase"]
 
+  constructor (private cropsApi : CropsService){
+    this.dataSource = new MatTableDataSource<Crop>();
+  }
+
+  getCrops(){
+    this.cropsApi.getList().subscribe((response : any) => {
+      this.dataSource.data = response;
+      this.dataSource.data = this.dataSource.data.filter((crop) => crop.state == "active")
+    })
+  }
+
+  ngOnInit() {
+    this.getCrops();
+  }
 }
---
+++ src/app/greenhouse/services/crops.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Crop} from "../model/crop";
+import {BaseService} from "../../shared/services/base.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CropsService extends BaseService<Crop> {
+
+  constructor(http: HttpClient) {
+    super(http)
+    this.resourceEndpoint = "/crops"
+  }
+}
