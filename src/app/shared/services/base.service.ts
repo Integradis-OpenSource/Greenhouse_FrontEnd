1d608d301209816beef7cc17f905661c29b997e4
CarloLSG <u20211a475@upc.edu.pe>
2023-10-24 01:52:18 -0500

    chore: added patch method to base service.

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -66,6 +66,11 @@ export class BaseService<T> {
       .pipe(retry(2), catchError(this.handleError));
   }
 
+  patch(id: any, item: any): Observable<T> {
+    return this.http.patch<T>(`${this.resourcePath()}/${id}`, JSON.stringify(item), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
   private resourcePath(): string {
     return `${this.basePath}${this.resourceEndpoint}`;
   }
