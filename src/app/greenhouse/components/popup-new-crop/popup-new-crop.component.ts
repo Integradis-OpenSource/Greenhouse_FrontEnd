431570e60141ee97069a3982d673c673ee592fc7
CarloLSG <u20211a475@upc.edu.pe>
2023-10-26 03:57:47 -0500

    chore(process-table): create a new crop redirects to the correct route and a new crop is created.

--- src/app/greenhouse/components/popup-new-crop/popup-new-crop.component.ts
+++ src/app/greenhouse/components/popup-new-crop/popup-new-crop.component.ts
@@ -1,6 +1,7 @@
 
 import {Component, Input} from '@angular/core';
 import {EventServiceService} from "../../services/event.service";
+import {CropsService} from "../../services/crops.service";
 @Component({
   selector: 'app-popup-new-crop',
   templateUrl: './popup-new-crop.component.html',
@@ -9,18 +10,31 @@ import {EventServiceService} from "../../services/event.service";
 export class PopupNewCropComponent {
   @Input() popupText = 'Do you want to start a new harvest? It will be recorded as start date ';
   date= Date.now()
+  id = 0;
+  currentDateTime = new Date();
+  currentDate = this.currentDateTime.toISOString().split('T')[0];
 
   popupVisible = false;
   isButtonDisabled = false;
 
-  constructor(private interactionService: EventServiceService) {
+  constructor(private interactionService: EventServiceService, private cropService: CropsService) {
 
   }
 
   activeObjectPopUpNewCrop() {
+    this.cropService.getList().subscribe((response: any) => {
+      this.id = response.length + 1;
+    });
     this.interactionService.activeObjectPopUpNewCrop();
     this.popupVisible = true;
     this.isButtonDisabled = true;
+  }
+
+  startPopUpNewCrop() {
+    this.cropService.create({organization_id: 1, start_date: this.currentDate, end_date: '', phase: 'Stock', state: 'active'}).subscribe((response: any) => {
+      console.log('Response',response)
+    });
+    this.popupVisible = false;
   }
   closePopUpNewCrop() {
     this.popupVisible = false;
