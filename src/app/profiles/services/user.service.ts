0a9dcdcbe2858bf5fc7fba4e2ebb174dea0fd260
CarloLSG <u20211a475@upc.edu.pe>
2023-11-17 01:00:59 -0500

    chore: added deployed web service connection for crops endpoint.

--- src/app/profiles/services/user.service.ts
+++ src/app/profiles/services/user.service.ts
@@ -3,6 +3,8 @@ import { BaseService } from '../../shared/services/base.service';
 import { User } from '../model/user';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
+import {AuthService} from "../../shared/services/auth.service";
+import {TokenStorageService} from "../../shared/services/tokenStorage.service";
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,8 @@ import { Observable, Subject } from 'rxjs';
 export class UserService extends BaseService<User> {
   private searchTerm$ = new Subject<string>(); // Subject para el término de búsqueda
 
-  constructor(http: HttpClient) {
-    super(http);
+  constructor(http: HttpClient, tokenStorageService: TokenStorageService) {
+    super(http, tokenStorageService);
     this.basePath = "https://my-json-server.typicode.com/CarloLSG/GreenhouseFakeAPI1"
     this.resourceEndpoint = "/users";
   }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from "rxjs";
 
-const AUTH_API = 'http://localhost:8080/api/v1/authentication/';
+//const AUTH_API = 'http://localhost:8080/api/v1/authentication/';
+const AUTH_API = 'https://greenhouse.zeabur.app/api/v1/authentication/';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
