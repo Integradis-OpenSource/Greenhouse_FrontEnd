df009fc8f803b3c3b8a6d4c3c0a35d307b9ebb42
CarloLSG <u20211a475@upc.edu.pe>
2023-11-19 22:06:52 -0500

    fix: missing , form the merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -111,7 +111,7 @@ export function createTranslateLoader(http: HttpClient) {
     MatSelectModule,
     MatPaginatorModule,
     MatSortModule,
-    AgChartsAngularModule
+    AgChartsAngularModule,
     ReactiveFormsModule,
   ],
   providers: [authInterceptorProviders],
